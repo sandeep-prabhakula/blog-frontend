b5feecab839d9f7a371f67fc47b09167db87fdf6
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2023-06-07 14:51:15 +0530

    fixed escape sequences

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -24,19 +24,19 @@ const about = () => {
         <div className={styles.item}>
           <h1 className={styles.title}>Who Am I?</h1>
           <p className={styles.desc}>
-            I'm a technology enthusiast and coding specialist that want to share my expertise and thoughts with the rest of the world. My objective is to help people understand the ever-changing world of technology, remain up to speed on the newest trends, and improve their coding abilities.
+            I&apos;m a technology enthusiast and coding specialist that want to share my expertise and thoughts with the rest of the world. My objective is to help people understand the ever-changing world of technology, remain up to speed on the newest trends, and improve their coding abilities.
             <br />
             <br />
-            I'm, at my heart, a technology and coding blog that seeks to be a trustworthy source of knowledge for computer lovers of all levels. I select and develop information on a variety of topics, such as new technology updates, emerging and trending technologies, and the principles of Data Structures and Algorithms (DSA).
+            I&apos;m, at my heart, a technology and coding blog that seeks to be a trustworthy source of knowledge for computer lovers of all levels. I select and develop information on a variety of topics, such as new technology updates, emerging and trending technologies, and the principles of Data Structures and Algorithms (DSA).
           </p>
         </div>
         <div className={styles.item}>
           <h1 className={styles.title}>What We Do?</h1>
           <p className={styles.desc}>
-            I'm committed to providing you with the most up-to-date information and insights into the ever-changing realm of technology. I scours the internet landscape for high-quality articles covering a wide range of topics, from new technology updates to emerging and trending technologies.
+            I&apos;m committed to providing you with the most up-to-date information and insights into the ever-changing realm of technology. I scours the internet landscape for high-quality articles covering a wide range of topics, from new technology updates to emerging and trending technologies.
             <br />
             <br />
-            I realise how critical it is to stay ahead in this continuously changing digital world. That is why my blog is your one-stop shop for all things technological. We offer something for everyone, whether you're a seasoned professional or an inquisitive novice. Our meticulously prepared articles take deep dives into a variety of tech fields, including detailed explanations, tutorials, and real-world applications.
+            I realise how critical it is to stay ahead in this continuously changing digital world. That is why my blog is your one-stop shop for all things technological. We offer something for everyone, whether you&apos;re a seasoned professional or an inquisitive novice. Meticulously prepared articles take deep dives into a variety of tech fields, including detailed explanations, tutorials, and real-world applications.
           </p>
           <Button url="/contact" text="Contact" />
         </div>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -6,7 +6,7 @@ import Button from "../../components/button/Button"
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image
