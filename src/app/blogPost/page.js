2d5fbb4c1b12ad9e5ef46917567b1c99d3de6de6
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2023-06-22 18:43:56 +0530

    added pagination

--- src/app/blogPost/page.js
+++ src/app/blogPost/page.js
@@ -1,40 +1,57 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './page.module.css'
-async function getData() {
-  const res = await fetch("https://sandeep-prabhakula-blog-backend.up.railway.app/get-all-blogs", {
-    cache: "no-store",
-  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+const Blog = () => {
+  const [blogs, setBlogs] = useState([])
+  const [pageNumber, setPageNumber] = useState(0)
+  useEffect(() => {
+    fetch(`https://sandeep-prabhakula-blog-backend.up.railway.app/get-all-blogs?pageNumber=${pageNumber}&pageSize=5`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBlogs(data)
+      }
+      );
+  }, [pageNumber])
+
+  const nextPage = async () => {
+    setPageNumber(prevActiveStep => prevActiveStep + 1)
   }
-
-  return res.json();
-}
-
-const Blog = async () => {
-  let data = await getData();
+  const prevPage = async () => {
+    setPageNumber(prevActiveStep => prevActiveStep - 1)
+  }
+  if (blogs.length === 0) return <h3 className={styles.desc}>No blogs found</h3>
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
+      {blogs.map((item) => (
         <Link href={`/blogPost/${item.id}`} className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
             <Image
               src={item.image}
               alt=""
-              width={400}
-              height={250}
+              width={300}
+              height={200}
               className={styles.image}
             />
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.description.substring(0,125)}</p>
+            <p className={styles.desc}>{item.description.substring(0, 125)}</p>
           </div>
         </Link>
       ))}
+
+      <div className={styles.pagination}>
+
+        <button className={styles.nextBtn} style={{ display: `${pageNumber === 0 ? 'none' : 'block'}` }} onClick={prevPage}>
+          Previous
+        </button>
+        <button className={styles.nextBtn} onClick={nextPage}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
