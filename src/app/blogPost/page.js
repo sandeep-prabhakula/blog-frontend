ce7a600268a175d6a1f4797904077cb98b369f9c
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-05-01 17:39:16 +0530

    deletion modal added

--- src/app/blogPost/page.js
+++ src/app/blogPost/page.js
@@ -14,13 +14,15 @@ const Blog = () => {
   const [blogs, setBlogs] = useState([])
   const [pageNumber, setPageNumber] = useState(0)
   const [loading, setLoading] = useState(true);
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [deletingBlogId, setDeletingBlogID] = useState('')
   useEffect(() => {
     async function fetchBlogs() {
       try {
         // PROD
 
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-all-blogs?pageNumber=${pageNumber}&pageSize=5`)
-        
+
         // TEST
 
         // const res = await fetch(`http://localhost:8080/get-all-blogs?pageNumber=${pageNumber}&pageSize=5`, {
@@ -68,18 +70,32 @@ const Blog = () => {
   // Delete Blog
 
   const deleteBlog = async (e) => {
-    const blogId = e.currentTarget.getAttribute('blog-id')
+    // const blogId = e.currentTarget.getAttribute('blog-id')
+    const blogId = deletingBlogId
     const url = `${process.env.NEXT_PUBLIC_API_URL}/delete-blog/${blogId}`
     // const url = `http://localhost:8080/delete-blog/${blogId}`
-    const res = await fetch(url, {
-      method:'DELETE',
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${JSON.parse(window.sessionStorage.getItem('currentUser')).jwtToken}`,
-        'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Methods': 'DELETE'
-      }
-    })
+    try {
+
+      await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${JSON.parse(window.sessionStorage.getItem('currentUser')).jwtToken}`,
+          'Access-Control-Allow-Origin': "*",
+          'Access-Control-Allow-Methods': 'DELETE'
+        }
+      })
+      setDeletingBlogID('')
+      setShowDeleteModal(!showDeleteModal)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Open and Close modals
+  const modalPopup = (e) => {
+    setDeletingBlogID(e.currentTarget.getAttribute('blog-id'))
+    setShowDeleteModal(!showDeleteModal)
   }
 
   const optimizedSearch = useCallback(debounce(onTextChangeListener), [])
@@ -98,6 +114,25 @@ const Blog = () => {
   </div>
   return (
     <div className={styles.mainContainer}>
+      {
+        showDeleteModal &&
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalWrapper}>
+            <div className={styles.modal}>
+              <div className={`${styles.modalHeader} ${blogTitleFont.className}`}>
+                <a href="#" onClick={modalPopup}>
+                  X
+                </a>
+              </div>
+              <div className={styles.modalBody}>
+                <h3 className={`${blogTitleFont.className}`}>Deletion alert</h3>
+                <h3 className={`${blogDescriptionFont.className}`}>Do you really want to delete this blog?</h3>
+                <button className={styles.nextBtn} onClick={deleteBlog}>Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
       <div className={styles.searchDiv}>
         <input
           placeholder='Search here'
@@ -111,42 +146,42 @@ const Blog = () => {
             {searchedblogs.map((item) => (
               <div key={item.id}>
                 {window.sessionStorage.getItem('currentUser') ? <Link href={{
-            pathname: '/edit',
-            query: {
-              id: item.id
-            }
-          }}><div>
-              <Image
-                src='/images/editBlog.svg'
-                alt='edit'
-                width={48}
-                height={48} />
-            </div></Link> : <></>}
-          {window.sessionStorage.getItem('currentUser') ? <div >
-            <Image
-              src='/images/deleteBlog.svg'
-              alt='delete'
-              width={48}
-              height={48} 
-              onClick={deleteBlog} 
-              blog-id={item.id}/>
-          </div> : <></>}
-              <Link href={`/blogPost/${item.id}`} className={styles.container} >
-
-                <div className={styles.imageContainer}>
+                  pathname: '/edit',
+                  query: {
+                    id: item.id
+                  }
+                }}>
+                    <Image
+                      src='/images/editBlog.svg'
+                      alt='edit'
+                      width={48}
+                      height={48} />
+                  </Link> : <></>}
+                {window.sessionStorage.getItem('currentUser') ? <div >
                   <Image
-                    src={item.image}
-                    alt=""
-                    width={300}
-                    height={200}
-                    className={styles.image}
-                  />
-                </div>
-                <div className={styles.content}>
-                  <h1 className={`${styles.title} ${blogTitleFont.className}`}>{item.title}</h1>
-                  <p className={`${styles.desc} ${blogDescriptionFont.className}`}>{item.description.substring(0, 125)}</p>
-                </div>
-              </Link>
+                    src='/images/deleteBlog.svg'
+                    alt='delete'
+                    width={48}
+                    height={48}
+                    onClick={deleteBlog}
+                    blog-id={item.id} />
+                </div> : <></>}
+                <Link href={`/blogPost/${item.id}`} className={styles.container} >
+
+                  <div className={styles.imageContainer}>
+                    <Image
+                      src={item.image}
+                      alt=""
+                      width={300}
+                      height={200}
+                      className={styles.image}
+                    />
+                  </div>
+                  <div className={styles.content}>
+                    <h1 className={`${styles.title} ${blogTitleFont.className}`}>{item.title}</h1>
+                    <p className={`${styles.desc} ${blogDescriptionFont.className}`}>{item.description.substring(0, 125)}</p>
+                  </div>
+                </Link>
               </div>
             ))}
 
@@ -158,26 +193,26 @@ const Blog = () => {
       </div>
       {blogs.map((item) => (<div key={item.id}>
         {window.sessionStorage.getItem('currentUser') ? <Link href={{
-            pathname: '/edit',
-            query: {
-              id: item.id
-            }
-          }}><div>
-              <Image
-                src='/images/editBlog.svg'
-                alt='edit'
-                width={48}
-                height={48} />
-            </div></Link> : <></>}
-          {window.sessionStorage.getItem('currentUser') ? <div >
+          pathname: '/edit',
+          query: {
+            id: item.id
+          }
+        }}>
             <Image
-              src='/images/deleteBlog.svg'
-              alt='delete'
+              src='/images/editBlog.svg'
+              alt='edit'
               width={48}
-              height={48} 
-              onClick={deleteBlog} 
-              blog-id={item.id}/>
-          </div> : <></>}
+              height={48} />
+          </Link> : <></>}
+        {window.sessionStorage.getItem('currentUser') ? <div >
+          <Image
+            src='/images/deleteBlog.svg'
+            alt='delete'
+            width={48}
+            height={48}
+            onClick={modalPopup}
+            blog-id={item.id} />
+        </div> : <></>}
         <Link href={`/blogPost/${item.id}`} className={styles.container} >
 
           <div className={styles.imageContainer}>
@@ -189,11 +224,13 @@ const Blog = () => {
               className={styles.image}
             />
           </div>
-          
+
           <div className={styles.content}>
             <h1 className={`${styles.title} ${blogTitleFont.className}`}>{item.title}</h1>
-            <p className={`${styles.desc} ${blogDescriptionFont.className}`}>{item.description.substring(0, 125)}</p>
+            <p className={`${styles.desc} ${blogDescriptionFont.className}`}>{item.description.substring(0, 125)}...</p>
           </div>
+
+
         </Link>
       </div>
       ))}
