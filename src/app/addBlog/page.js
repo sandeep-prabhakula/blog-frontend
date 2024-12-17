28d45aa9e482299d510f6784ab582b13f430ddcc
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-12-17 20:09:23 +0530

    implemented rich text editor

--- src/app/addBlog/page.js
+++ src/app/addBlog/page.js
@@ -1,91 +1,149 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import styles from './page.module.css'
-import { useRouter } from 'next/navigation'
-import localFont from 'next/font/local'
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+import localFont from "next/font/local";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-const imgTitleFont = localFont({ src: '../../fonts/osiris.otf' })
+const imgTitleFont = localFont({ src: "../../fonts/osiris.otf" });
 const AddBlog = () => {
-    const router = useRouter()
+  const router = useRouter();
+  const [content, setContent] = useState("");
 
-    const [uid, setUid] = useState({})
+  const [uid, setUid] = useState({});
 
-    const [success, setSuccess] = useState('');
-    const [error, setError] = useState('')
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
 
-    const [blog, setBlog] = useState({
-        title: "",
-        description: "",
-        postedAt: "",
-        image: "",
-    })
-    useEffect(() => {
-        if (JSON.parse(window.sessionStorage.getItem('currentUser')).userData.roles !== "ROLE_ADMIN") {
-            router.push('/login')
-        } else {
-            const temp = JSON.parse(window.sessionStorage.getItem('currentUser'))
-            setUid(temp)
-        }
-    }, [])
+  const [blog, setBlog] = useState({
+    title: "",
+    description: "",
+    postedAt: "",
+    image: "",
+  });
 
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6,false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' },{'indent': '-1'}, {'indent': '+1'}],
+      [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }],
+      ["link", "image"]
+    ],
+  };
+  
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'link',
+    'image',
+    'video',
+    'formula'
+  ];
 
-    const handleChange = (e) => {
-        setBlog((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value
-        }))
+  useEffect(() => {
+    if (
+      JSON.parse(window.sessionStorage.getItem("currentUser")).userData
+        .roles !== "ROLE_ADMIN"
+    ) {
+      router.push("/login");
+    } else {
+      const temp = JSON.parse(window.sessionStorage.getItem("currentUser"));
+      setUid(temp);
     }
+  }, []);
 
-    const addNewBLog = async (e) => {
-        setError('')
-        setSuccess('')
-        const payload = blog
-        e.preventDefault()
-        if (payload.title !== '' && payload.description !== "" && payload.postedAt !== '' && payload.image !== '') {
-            try {
+  const handleChange = (e) => {
+    setBlog((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-                await fetch(`${process.env.NEXT_PUBLIC_API_URL}/add-blog`, {
-                    method: 'POST',
-                    body: JSON.stringify(payload),
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Authorization": `Bearer ${uid.jwtToken}`,
-                        'Access-Control-Allow-Origin': "*",
-                        'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS'
-                    }
-                })
-                setSuccess('Blog Posted Successfully')
-            } catch (error) {
-                setError('Something went Wrong')
-            }
-        }else {
-            setError('All fields are mandatory')
-        }
-
+  const addNewBLog = async (e) => {
+    setError("");
+    setSuccess("");
+    blog["description"] = content;
+    const payload = blog;
+    e.preventDefault();
+    if (
+      payload.title !== "" &&
+      payload.description !== "" &&
+      payload.postedAt !== "" &&
+      payload.image !== ""
+    ) {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/add-blog`, {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${uid.jwtToken}`,
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
+          },
+        });
+        setSuccess("Blog Posted Successfully");
+      } catch (error) {
+        setError("Something went Wrong");
+      }
+    } else {
+      setError("All fields are mandatory");
     }
-    return (
-        <div className={styles.container}>
-            <h1 className={`${styles.title} ${imgTitleFont.className}`}>Add new blog</h1>
-            {success && <small className={styles.success}>{success}</small>}
-            {error && <small className={styles.error}>{error}</small>}
-            <form className={styles.form}>
-                <input type="text" placeholder="title" className={styles.input} required onChange={handleChange} name='title' />
-                <input type="text" placeholder="imageURL" className={styles.input} required onChange={handleChange} name='image' />
-                <input type="date" placeholder="dd-mm-yyyy" className={styles.input} required onChange={handleChange} name='postedAt' />
-                <textarea
-                    className={styles.textArea}
-                    placeholder="description"
-                    cols="30"
-                    rows="10"
-                    required
-                    onChange={handleChange}
-                    name='description'
-                ></textarea>
+  };
+  return (
+    <div className={styles.container}>
+      <h1 className={`${styles.title} ${imgTitleFont.className}`}>
+        Add new blog
+      </h1>
+      {success && <small className={styles.success}>{success}</small>}
+      {error && <small className={styles.error}>{error}</small>}
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="title"
+          className={styles.input}
+          required
+          onChange={handleChange}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="imageURL"
+          className={styles.input}
+          required
+          onChange={handleChange}
+          name="image"
+        />
+        <input
+          type="date"
+          placeholder="dd-mm-yyyy"
+          className={styles.input}
+          required
+          onChange={handleChange}
+          name="postedAt"
+        />
+        <ReactQuill
+        theme="snow"
+          value={content}
+          onChange={setContent}
+          modules={modules}
+      formats={formats}
+        />
 
-                <button className={styles.submitBtn} onClick={addNewBLog}>Send</button>
-            </form>
-        </div>
-    )
-}
+        <button className={styles.submitBtn} onClick={addNewBLog}>
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default AddBlog
+export default AddBlog;
--- src/app/blogPost/[id]/page.js
+++ src/app/blogPost/[id]/page.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Image from 'next/image';
-import styles from './page.module.css'
-import Head from 'next/head';
-import Link from 'next/link';
-import localFont from 'next/font/local'
-import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/default-highlight';
-import { androidstudio } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import React from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+import Head from "next/head";
+import Link from "next/link";
+import localFont from "next/font/local";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import { androidstudio } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-
-const blogTitleFont = localFont({ src: "../../../fonts/BebasNeue-Regular.otf" })
-const aboutAuthorFont = localFont({ src: "../../../fonts/NexaExtraLight.ttf" })
-const descriptionFont = localFont({ src: "../../../fonts/OpenSans.ttf" })
-
+const blogTitleFont = localFont({
+  src: "../../../fonts/BebasNeue-Regular.otf",
+});
+const aboutAuthorFont = localFont({ src: "../../../fonts/NexaExtraLight.ttf" });
+const descriptionFont = localFont({ src: "../../../fonts/OpenSans.ttf" });
 
 async function getData(id) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog/${id}`, {
@@ -27,10 +27,10 @@ async function getData(id) {
 async function test(id) {
   const res = await fetch(`http://localhost:3000/dummy.json`, {
     cache: "no-store",
-  })
-  const data = await res.json()
-  const obj = data[id - 1]
-  return obj
+  });
+  const data = await res.json();
+  const obj = data[id - 1];
+  return obj;
 }
 
 export async function generateMetadata({ params }) {
@@ -38,70 +38,86 @@ export async function generateMetadata({ params }) {
   // const post =await test(params.id)
 
   //production
-  const post = await getData(params.id)
+  const post = await getData(params.id);
   return {
     title: post.title,
     description: post.description,
     openGraph: {
-      images: [{
-        url: post.image,
-        width: 300,
-        height: 200
-      }],
+      images: [
+        {
+          url: post.image,
+          width: 300,
+          height: 200,
+        },
+      ],
       title: post.title,
       description: post.description,
     },
-    keywords: post.title.split(' ')
+    keywords: post.title.split(" "),
   };
 }
 
 const Blog = async ({ params }) => {
-  let key = 0
+  let key = 0;
 
   //production
-  const data = await getData(params.id)
+  const data = await getData(params.id);
+
+  const isNewFormat = data.description.startsWith("<");
 
   //testing
   // const data =await test(params.id)
-
 
   return (
     <div className={styles.container}>
       <Head>
         <title>{data.title}</title>
-        <meta name='title' content={`${data.title}`} />
-        <meta name='description' content={`${data.description}`} />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content={`${data.title}`} />
-        <meta property='og:description' content={`${data.description.substring(0, 101) + "..."}`} />
-        <meta property='og:image' content={`${data.image}`} />
+        <meta name="title" content={`${data.title}`} />
+        <meta name="description" content={`${data.description}`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`${data.title}`} />
+        <meta
+          property="og:description"
+          content={`${data.description.substring(0, 101) + "..."}`}
+        />
+        <meta property="og:image" content={`${data.image}`} />
         <meta property="og:locale" content="en_US" />
-        <meta property='og:url' content={`https://codeverse-chronicles.netlify.app/${data.id}`} />
-        <meta property='og:site_name' content='https://codeverse-chronicles.netlify.app' />
+        <meta
+          property="og:url"
+          content={`https://codeverse-chronicles.netlify.app/${data.id}`}
+        />
+        <meta
+          property="og:site_name"
+          content="https://codeverse-chronicles.netlify.app"
+        />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property='twitter:title' content={`${data.title}`} />
-        <meta property='twitter:url' content={`https://codeverse-chronicles.netlify.app/${data.id}`} />
-        <meta property='twitter:image' content={`${data.image}`} />
-        <meta property='twitter:description' content={`${data.description}`} />
-
-
+        <meta property="twitter:title" content={`${data.title}`} />
+        <meta
+          property="twitter:url"
+          content={`https://codeverse-chronicles.netlify.app/${data.id}`}
+        />
+        <meta property="twitter:image" content={`${data.image}`} />
+        <meta property="twitter:description" content={`${data.description}`} />
       </Head>
       <div className={`${styles.path} ${aboutAuthorFont.className}`}>
-        <Link href='/'>
+        <Link href="/">
           <small>Codeverse Chronicles &gt; </small>
         </Link>
-        <Link href='/blogPost'>
+        <Link href="/blogPost">
           <small>Blogs &gt; </small>
         </Link>
         <small> {data.title} </small>
       </div>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={`${styles.title} ${blogTitleFont.className}`}>{data.title}</h1>
+          <h1 className={`${styles.title} ${blogTitleFont.className}`}>
+            {data.title}
+          </h1>
           <div className={`${styles.author} ${aboutAuthorFont.className}`}>
-            <small className={styles.username}>Author : Sandeep Prabhakula | Published on: {data.postedAt}</small>
+            <small className={styles.username}>
+              Author : Sandeep Prabhakula | Published on: {data.postedAt}
+            </small>
           </div>
-
         </div>
       </div>
       <hr />
@@ -116,34 +132,53 @@ const Blog = async ({ params }) => {
           />
         </div>
         <div className={styles.content}>
-        {data.description.split('\n\n').map((paragraph) => {
+          {isNewFormat?<div dangerouslySetInnerHTML={{__html:data.description}}/>:data.description.split("\n\n").map((paragraph) => {
+            // Heading
+            if (paragraph.endsWith(":"))
+              return (
+                <h3 className={`${blogTitleFont.className}`}>
+                  {paragraph}
+                  <br />
+                </h3>
+              );
 
-          // Heading
-          if (paragraph.endsWith(":")) return <h3 className={`${blogTitleFont.className}`}>{paragraph}<br /></h3>
+            // Code
+            if (paragraph.startsWith("```"))
+              return (
+                <SyntaxHighlighter style={androidstudio} showLineNumbers>
+                  {paragraph.slice(3, -3)}
+                </SyntaxHighlighter>
+              );
 
-          // Code
-          if (paragraph.startsWith("```")) return <SyntaxHighlighter style={androidstudio} showLineNumbers >
-            {paragraph.slice(3, -3)}
-          </SyntaxHighlighter>
+            // Images
+            if (paragraph.startsWith("["))
+              return (
+                <div className={styles.imageContainer}>
+                  <Image
+                    src={paragraph.slice(1, -1)}
+                    alt=""
+                    width={500}
+                    height={500}
+                    className={styles.image}
+                  />
+                </div>
+              );
 
-          // Images
-          if (paragraph.startsWith("[")) return <div className={styles.imageContainer}>
-            <Image
-              src={paragraph.slice(1, -1)}
-              alt=""
-              width={500}
-              height={500}
-              className={styles.image}
-            />
-          </div>
-
-          // Description
-          return <p className={`${styles.text} ${descriptionFont.className}`} key={key++}>{paragraph}<br /></p>
-        })}
+            // Description
+            return (
+              <p
+                className={`${styles.text} ${descriptionFont.className}`}
+                key={key++}
+              >
+                {paragraph}
+                <br />
+              </p>
+            );
+          })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
