98a7472c07dee4ec93b0f08e11a39ef6af136854
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2026-04-11 19:47:05 +0530

    fixed the .txt routing error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
         domains: ['drive.google.com','firebasestorage.googleapis.com'],
       },
